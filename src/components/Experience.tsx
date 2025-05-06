2ccb8f28e1489527b266f3927cf9eee48765a4e6
Vrushabh Veer <veervrushabh@gmail.com>
2025-05-06 16:30:38 +0530

    update

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import React, { useEffect } from "react";
-import latitude from "@/assets/latitude-logo.webp";
+import globemind from "@/assets/globeminds_logo.png";
 import "aos/dist/aos.css";
 import Aos from "aos";
 
@@ -28,29 +28,29 @@ const Experience = () => {
             <div className="flex flex-col md:flex-row items-center justify-between">
               <div className="flex flex-col md:flex-row float-left items-center gap-4">
                 <a
-                  href="https://latitudetechnolabs.com"
+                  href=""
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <Image
-                    className="w-20"
-                    src={latitude}
-                    alt="latitude-logo"
+                    className="w-24"
+                    src={globemind}
+                    alt="globeminds_logo"
                     loading="lazy"
                   />
                 </a>
                 <h2 className="font-semibold md:text-lg text-left">
-                  React JS Developer at Latitude Technolabs
+                  React JS Developer at Globeminds Technologies
                 </h2>
               </div>
               <div>
-                <p className="font-medium">March 2023 - Present</p>
+                <p className="font-medium">Oct 2023 - Present</p>
               </div>
             </div>
 
             <div className="mt-5">
               <p className="text-[#3f3f46]">
-                As a React JS Developer at Latitude Technolabs Pvt Ltd, played a
+                As a React JS Developer at Globeminds Technologies Pvt Ltd, played a
                 key role in developing and maintaining dynamic web applications,
                 focusing on creating responsive and engaging user interfaces
                 with React JS. By working closely with cross-functional teams,
